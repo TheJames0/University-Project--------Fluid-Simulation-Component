eff1f88b37dcf177753586d9935f120a0175587e
TheJames0 <101518239+TheJames0@users.noreply.github.com>
2023-01-25 14:07:21 +0000

    Added 1d to 2d array conversion

--- main.rs
+++ main.rs
@@ -9,7 +9,12 @@ struct Fluid
     velocity_y:[f32; SIM_SIZE * SIM_SIZE],
     
 }
-fn exportdensity()
+fn getArrayValue(x:u32,y:u32,array:&mut[f32]) -> f32 {
+    let size: u32 = SIM_SIZE as u32;
+    let index: usize = ((y*size) + x) as usize;
+    return  array[index];
+}
+fn exportdensity(density_array:&mut[f32])
 {
     let size: u32 = SIM_SIZE as u32;
     let mut image: RgbImage = RgbImage::new(100, 100);
@@ -18,8 +23,8 @@ fn exportdensity()
     {
         for y in 0..size
         {
-            
-            *image.get_pixel_mut(x, y) = image::Rgb([255,0,0]);
+            let val: u8 = (getArrayValue(x, y, density_array)*255.0) as u8;
+            *image.get_pixel_mut(x, y) = image::Rgb([val,0,0]);
             
         }
     }
@@ -28,19 +33,22 @@ fn exportdensity()
 }
 fn main()
 {
-    exportdensity();
+    
     //Init mutable frame count int
     let mut frame_count = 0;
     //Init Fluid Array values
     let mut _fluid = Fluid
     {
-        density:[0.0; SIM_SIZE*SIM_SIZE],
+        density:[125.0; SIM_SIZE*SIM_SIZE],
         velocity_x:[0.0; SIM_SIZE*SIM_SIZE],
         velocity_y:[0.0; SIM_SIZE * SIM_SIZE],
     };
+    exportdensity(&mut _fluid.density);
     //Main Iteration Loop
     while frame_count < 100 
     {
+        //Export density
+        
         //Increment frame count
         frame_count = frame_count + 1;
     }
